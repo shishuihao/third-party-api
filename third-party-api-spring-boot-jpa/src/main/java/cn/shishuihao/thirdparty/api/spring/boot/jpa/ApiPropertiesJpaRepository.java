6756edff4b144b9b9f8c2c8dd5914977ec371f71
shishuihao <1285173409@qq.com>
2021-02-27 11:01:39 +0800

    refactor(third-party-api-spring-boot-jpa):

--- third-party-api-spring-boot-jpa/src/main/java/cn/shishuihao/thirdparty/api/spring/boot/jpa/ApiPropertiesJpaRepository.java
+++ third-party-api-spring-boot-jpa/src/main/java/cn/shishuihao/thirdparty/api/spring/boot/jpa/ApiPropertiesJpaRepository.java
@@ -4,7 +4,6 @@ import cn.shishuihao.thirdparty.api.core.ApiProperties;
 import cn.shishuihao.thirdparty.api.core.ApiPropertiesRepository;
 import cn.shishuihao.thirdparty.api.spring.boot.jpa.entity.ApiPropertiesEntity;
 import cn.shishuihao.thirdparty.api.spring.boot.jpa.repository.ApiPropertiesEntityJpaRepository;
-import org.springframework.cglib.beans.BeanMap;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -20,29 +19,25 @@ public class ApiPropertiesJpaRepository implements ApiPropertiesRepository {
         this.jpaRepository = jpaRepository;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public void add(final ApiProperties apiProperties) {
-        Optional<ApiPropertiesEntity> optional = jpaRepository.findByPropertiesId(apiProperties.id());
-        ApiPropertiesEntity entity;
-        if (optional.isPresent()) {
-            entity = optional.get();
-            entity.setProperties(BeanMap.create(apiProperties));
-            entity.setGmtModified(LocalDateTime.now());
-            jpaRepository.save(entity);
-        } else {
-            entity = ApiPropertiesEntity.from(apiProperties);
-        }
+        ApiPropertiesEntity entity = jpaRepository.findByPropertiesId(apiProperties.id())
+                .map(it -> {
+                    it.setProperties(apiProperties);
+                    it.setGmtModified(LocalDateTime.now());
+                    return it;
+                })
+                .orElseGet(() -> ApiPropertiesEntity.from(apiProperties));
         jpaRepository.save(entity);
     }
 
     @Override
     public Optional<ApiProperties> getById(final String id) {
-        return jpaRepository.findByPropertiesId(id).map(ApiPropertiesEntity::toApiProperties);
+        return jpaRepository.findByPropertiesId(id).map(ApiPropertiesEntity::getProperties);
     }
 
     @Override
     public Optional<ApiProperties> getApiProperties(final String channelId, final String propertiesId) {
-        return jpaRepository.findByChannelIdAndPropertiesId(channelId, propertiesId).map(ApiPropertiesEntity::toApiProperties);
+        return jpaRepository.findByChannelIdAndPropertiesId(channelId, propertiesId).map(ApiPropertiesEntity::getProperties);
     }
 }
--- third-party-api-spring-boot-jpa/src/main/java/cn/shishuihao/thirdparty/api/spring/boot/jpa/entity/ApiPropertiesEntity.java
+++ third-party-api-spring-boot-jpa/src/main/java/cn/shishuihao/thirdparty/api/spring/boot/jpa/entity/ApiPropertiesEntity.java
@@ -1,16 +1,10 @@
 package cn.shishuihao.thirdparty.api.spring.boot.jpa.entity;
 
 import cn.shishuihao.thirdparty.api.core.ApiProperties;
-import cn.shishuihao.thirdparty.api.core.ApiRegistry;
-import cn.shishuihao.thirdparty.api.core.exception.PropertiesCastException;
-import org.springframework.beans.BeanUtils;
-import org.springframework.cglib.beans.BeanMap;
+import cn.shishuihao.thirdparty.api.spring.boot.jpa.converter.ApiPropertiesJacksonAttributeConverter;
 
 import javax.persistence.*;
-import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * @author shishuihao
@@ -24,6 +18,7 @@ public class ApiPropertiesEntity extends BaseEntity {
     public static final String TABLE_NAME = "`api_properties`";
     public static final String COLUMN_CHANNEL_ID = "`channel_id`";
     public static final String COLUMN_PROPERTIES_ID = "`properties_id`";
+    public static final String COLUMN_PROPERTIES = "`properties`";
     /**
      * channel id
      */
@@ -37,34 +32,17 @@ public class ApiPropertiesEntity extends BaseEntity {
     /**
      * properties
      */
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = ApiPropertyEntity.TABLE_NAME, joinColumns = @JoinColumn(name = COLUMN_PROPERTIES_ID))
-    @MapKeyColumn(name = ApiPropertyEntity.COLUMN_KEY)
-    @Column(name = ApiPropertyEntity.COLUMN_VALUE, length = 10240)
-    private Map<String, String> properties;
+    @Convert(converter = ApiPropertiesJacksonAttributeConverter.class)
+    @Column(name = COLUMN_PROPERTIES, length = 10240)
+    private ApiProperties properties;
 
-    @SuppressWarnings("unchecked")
     public static ApiPropertiesEntity from(ApiProperties apiProperties) {
         ApiPropertiesEntity po = new ApiPropertiesEntity();
         po.setChannelId(apiProperties.channelId());
         po.setPropertiesId(apiProperties.id());
-        po.setProperties(BeanMap.create(apiProperties));
+        po.setProperties(apiProperties);
         po.setGmtModified(LocalDateTime.now());
         return po;
-    }
-
-    public ApiProperties toApiProperties() {
-        try {
-            ApiProperties apiProperties = ApiRegistry.INSTANCE.getApiChannelOrThrow(channelId).newProperties();
-            // 拷贝自身
-            BeanUtils.copyProperties(this, apiProperties);
-            // 拷贝map
-            BeanMap beanMap = BeanMap.create(apiProperties);
-            Optional.ofNullable(this.properties).ifPresent(it -> it.forEach(beanMap::put));
-            return apiProperties;
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new PropertiesCastException(e);
-        }
     }
 
     public String getChannelId() {
@@ -83,11 +61,11 @@ public class ApiPropertiesEntity extends BaseEntity {
         this.propertiesId = propertiesId;
     }
 
-    public Map<String, String> getProperties() {
+    public ApiProperties getProperties() {
         return properties;
     }
 
-    public void setProperties(Map<String, String> properties) {
+    public void setProperties(ApiProperties properties) {
         this.properties = properties;
     }
 }

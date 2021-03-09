4d9dc31a4cbabdea511ca40580b4b5242590f709
shishuihao <1285173409@qq.com>
2021-03-09 23:43:51 +0800

    refactor(third-party-api-pay-weixin-sdk): WxPayOrderQueryRequest

--- third-party-api-pay-weixin-sdk/src/main/java/cn/shishuihao/thirdparty/api/pay/weixin/sdk/request/WxPayOrderQueryRequest.java
+++ third-party-api-pay-weixin-sdk/src/main/java/cn/shishuihao/thirdparty/api/pay/weixin/sdk/request/WxPayOrderQueryRequest.java
@@ -1,5 +1,6 @@
 package cn.shishuihao.thirdparty.api.pay.weixin.sdk.request;
 
+import cn.shishuihao.thirdparty.api.pay.weixin.sdk.annotation.WxParameter;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
@@ -14,20 +15,24 @@ import lombok.experimental.SuperBuilder;
 public class WxPayOrderQueryRequest extends AbstractWxPayXmlRequest {
     /**
      * 微信订单号.
-     * String(32)
-     * 1009660380201506130728806387
-     * 微信的订单号，建议优先使用
-     * 商户系统内部订单号，要求32个字符内，只能是数字、大小写字母_-|*且在同一个商户号下唯一。详见商户订单号
+     * 微信订单号和商户订单号二选一
      */
+    @WxParameter(
+            name = "微信订单号",
+            required = false, type = "String(32)",
+            example = "013467007045764",
+            desc = "微信的订单号，优先使用")
     @XStreamAlias("transaction_id")
     private final String transactionId;
     /**
      * 商户订单号.
-     * out_trade_no
-     * String(32)
-     * 20150806125346
-     * 商户系统内部订单号，要求32个字符内，只能是数字、大小写字母_-|*@ ，且在同一个商户号下唯一。 详见商户订单号
+     * 微信订单号和商户订单号二选一
      */
+    @WxParameter(
+            name = "商户订单号",
+            required = false, type = "String(32)",
+            example = "1217752501201407033233368018",
+            desc = "商户系统内部订单号，要求32个字符内，只能是数字、大小写字母_-|*@ ，且在同一个商户号下唯一。 详见商户订单号")
     @XStreamAlias("out_trade_no")
     private final String outTradeNo;
 }

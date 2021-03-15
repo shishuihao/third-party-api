e38b81c8d4c2a8419edc98b505317225db3777d3
shishuihao <1285173409@qq.com>
2021-03-16 05:19:34 +0800

    feat(third-party-api-pay-spring-boot-web):

---
+++ third-party-api-pay-spring-boot-web/src/main/java/cn/shishuihao/thirdparty/api/pay/spring/boot/web/autoconfigure/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * pay spring boot web autoconfigure.
+ *
+ * @author shishuihao
+ * @version 1.0.0
+ */
+package cn.shishuihao.thirdparty.api.pay.spring.boot.web.autoconfigure;
---
+++ third-party-api-pay-spring-boot-web/src/main/java/cn/shishuihao/thirdparty/api/pay/spring/boot/web/controller/ThirdPartyApiPayController.java
@@ -0,0 +1,30 @@
+package cn.shishuihao.thirdparty.api.pay.spring.boot.web.controller;
+
+import cn.shishuihao.thirdparty.api.core.ApiRegistry;
+import cn.shishuihao.thirdparty.api.pay.request.CodePayApiRequest;
+import cn.shishuihao.thirdparty.api.pay.response.CodePayApiResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author shishuihao
+ * @version 1.0.0
+ */
+@RequestMapping("/api/v1/pay")
+public class ThirdPartyApiPayController {
+    /**
+     * code pay.
+     *
+     * @param request request
+     * @return response
+     */
+    @PostMapping("/code-pay")
+    public CompletableFuture<CodePayApiResponse> codePay(
+            @RequestBody final CodePayApiRequest request) {
+        return CompletableFuture.supplyAsync(() ->
+                ApiRegistry.INSTANCE.execute(request));
+    }
+}

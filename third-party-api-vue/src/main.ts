27d86429538d5d1e2b6d86969ede2d797064665b
shishuihao <1285173409@qq.com>
2021-03-22 02:16:49 +0800

    build(third-party-api-vue): ant-design-vue

--- third-party-api-vue/src/main.ts
+++ third-party-api-vue/src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+
+import Antd from 'ant-design-vue';
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
 const app = createApp(App)
+app.use(Antd)
 app.use(router)
 app.use(store)
 app.mount('#app')

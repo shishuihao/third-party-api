143f5ee9c1f6b98854d1ffd6988dc13d61e6d86f
shishuihao <1285173409@qq.com>
2021-03-27 03:25:55 +0800

    fix(third-party-api-pay-hsb): remove hsb

--- third-party-api-pay-ccb-hsb-sdk/src/main/java/cn/shishuihao/thirdparty/api/pay/ccb/hsb/sdk/domain/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * ccb hsb pay sdk domain.
- *
- * @author shishuihao
- * @version 1.0.0
- */
-package cn.shishuihao.thirdparty.api.pay.ccb.hsb.sdk.domain;
--- third-party-api-pay-ccb-hsb-sdk/src/main/java/cn/shishuihao/thirdparty/api/pay/ccb/hsb/sdk/request/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * ccb hsb pay sdk request.
- *
- * @author shishuihao
- * @version 1.0.0
- */
-package cn.shishuihao.thirdparty.api.pay.ccb.hsb.sdk.request;

30aa19cee6dbfab9727a71a29afe079f5de013c2
shishuihao <1285173409@qq.com>
2021-05-10 02:48:01 +0800

    feat(third-party-api-pay-domain): AdditionInfo

--- third-party-api-pay-domain/src/main/java/cn/shishuihao/thirdparty/api/pay/domain/merchant/bank_account_info/package-info.java
+++ third-party-api-pay-domain/src/main/java/cn/shishuihao/thirdparty/api/pay/domain/merchant/bank_account_info/package-info.java
@@ -1,5 +1,5 @@
 /**
- * 支付领域商户子域(pay domain merchant subdomain) 商户主体(merchant subject) 结算银行账户.
+ * 支付领域商户子域(pay domain merchant subdomain) 商户结算银行账户(merchant bank account info).
  *
  * @author shishuihao
  * @version 1.0.0
